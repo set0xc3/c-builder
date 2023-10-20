4262e99b5737e94c6b4b2d365ff580b2ef199100
set0xc3 <set0xc3@gmail.com>
2023-10-20 22:12:58 +0300

    точно завтра

--- assets/shaders/default_vert.glsl
+++ assets/shaders/default_vert.glsl
@@ -12,6 +12,6 @@ uniform mat4 u_projection;
 void
 main() 
 {
-	gl_Position = u_projection * u_view * u_model * vec4(a_position.xy, 0.0, 1.0f);
+	gl_Position = u_projection * u_model * vec4(a_position.xy, 0.0, 1.0f);
 	v_color = a_color;
 }
--- tests/test_gfx.c
+++ tests/test_gfx.c
@@ -18,11 +18,11 @@ main(void)
       = gfx_shader_create(str_lit("assets/shaders/default_vert.glsl"),
                           str_lit("assets/shaders/default_frag.glsl"));
 
-  f32         fov         = 45;
-  f32         aspect      = 1280.0f / 720.0f;
-  f32         near        = -0.01f;
-  f32         far         = 1.0f;
-  GFX_Camera *camera_view = camera_create(fov, aspect, near, far);
+  f32  fov               = 90;
+  f32  aspect            = viewport.width / viewport.height;
+  f32  near              = -1.0f;
+  f32  far               = 1.0f;
+  mat4 projection_matrix = mat4_orthographic(0.0f, 1280, 720, 0.0f, near, far);
 
   while (!is_quit) {
 
@@ -31,23 +31,17 @@ main(void)
         is_quit = true;
       }
       viewport = os_window_root_get()->rect;
-      camera_resize_on(camera_view, viewport.xy);
     }
 
     gfx_frame_begin();
 
-    mat4 projection_matrix = camera_view->perspective_matrix;
-    mat4 view_matrix       = camera_view->view_matrix;
-
-    vec3 model_pos    = vec3_init(0.0f, 0.0f, 0.0f);
     mat4 model_matrix = mat4_identity();
-    model_matrix      = mat4_translation(model_pos);
+    model_matrix      = mat4_translation(vec3_zero());
 
     gfx_shader_use(shader_default);
-    gfx_shader_mat4_set(shader_default, str_lit("u_model"), model_matrix);
-    gfx_shader_mat4_set(shader_default, str_lit("u_view"), view_matrix);
     gfx_shader_mat4_set(shader_default, str_lit("u_projection"),
                         projection_matrix);
+    gfx_shader_mat4_set(shader_default, str_lit("u_model"), model_matrix);
 
     gfx_quad_push(vec4_init(0, 0, 100, 100), vec4_init(1, 0, 1, 1));
 
